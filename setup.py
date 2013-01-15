a9451a63eadcd156ea829cb31f110b2590a792fb
Philipp Meier <pmeier82@googlemail.com>
2013-01-15 14:24:33 +0100

    added docutils and pybtex as requirement

--- setup.py
+++ setup.py
@@ -8,10 +8,10 @@ if __name__ == '__main__':
         name='sphinx-natbib',
         version='0.1',
         packages=['natbib'],
-        requires=['sphinx', 'pybtex'],
+        requires=['docutils', 'sphinx', 'pybtex'],
 
         # metadata
-        author='W. Nielson',
+        author='Weston Nielson',
         author_email='',
         maintainer='Philipp Meier',
         maintainer_email='pmeier82@gmail.com',
