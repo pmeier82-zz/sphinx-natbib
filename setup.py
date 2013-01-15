03766d45861fd11c12ec4c17c375d2b946efb796
Philipp Meier <pmeier82@googlemail.com>
2013-01-15 14:01:20 +0100

    initial commit with fork from bitbucket

---
+++ setup.py
@@ -0,0 +1,18 @@
+"""install script for sphinx-natbib"""
+
+from setuptools import setup
+
+if __name__ == '__main__':
+    setup(
+        #main
+        name='sphinx-natbib',
+        version='0.1',
+        packages=['natbib'],
+        requires=['sphinx'],
+
+        # metadata
+        author='W. Nielson',
+        author_email='',
+        maintainer='Philipp Meier',
+        maintainer_email='pmeier82@gmail.com',
+        description='sphinx extension to use bibtex files')
